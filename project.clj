f0da6066b689f7cb37f1b7c15e49ee024aa18b8b
Konrad Grzanek <kongra@gmail.com>
2016-01-27 22:58:16 +0100

    Moved to Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name   "Eclipse Public License"
             :url    "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure                 "1.7.0"]
+  :dependencies [[org.clojure/clojure                 "1.8.0"]
+
                  [org.clojure/math.combinatorics      "0.1.1"]
                  [org.clojure/math.numeric-tower      "0.0.4"]
                  [org.apache.commons/commons-lang3    "3.4"  ]
